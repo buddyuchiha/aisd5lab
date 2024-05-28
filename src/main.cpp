12615faf414bcf9702ba78af018b0a5908a73050
afrodetka <Pro100biv@mail.ru>
2024-05-28 15:21:13 +0400

    Переделал print, insert, search, решаю ошибку с хэш-функцией

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 template <typename K, typename T>
@@ -9,15 +10,24 @@ private:
 	struct HashPair {
 		K _key;
 		T _value;
+		HashPair* _next;
 		bool _filled;
-		HashPair(K key, T value) : _key(key), _value(value), _filled(false) {}
-		HashPair(): _key(K()), _value(T()), _filled(false) {}
+		HashPair(K key, T value) : _key(key), _value(value), _filled(true), _next(nullptr) {}
+		HashPair() : _key(K()), _value(T()), _filled(false), _next(nullptr) {}
 	};
 	vector<HashPair<K, T>> _data;
-	int _capacity;
+	size_t _capacity;
+	size_t hash_function(K key) {
+		size_t a = 333333333333333;
+		size_t w = sizeof(size_t) * 8;
+		size_t l = log2(_capacity);
+		size_t result = ((key * a) % static_cast<size_t>(pow(2, w))) >> (w - l);
+		cout << "Capacity: " << _capacity << endl;
+		return result;
+	}
 public:
 	HashTable(int capacity);
-	~HashTable();
+	~HashTable();  
 	HashTable(const HashTable& other);
 	void print();
 	void insert(K key, T value);
@@ -44,19 +54,42 @@ HashTable<K, T>::~HashTable()
 template<typename K, typename T>
 HashTable<K, T>::HashTable(const HashTable& other) : _data(other._data), _capacity(other._capacity)
 {
+
 }
 
 template<typename K, typename T>
 void HashTable<K, T>::print()
 {
-	for (int i = 0, i < _data.size(), i++) {
-		cout << Key: << _data[i]._key << 
+	for (int i = 0; i < _data.size(); i++) {
+		cout << "Bucket: " << i << endl;
+		HashPair<K, T>* temp = &_data[i];
+		while (temp) {
+			cout << "{" << "Key: " << temp->_key << " Value: " << temp->_value << "}" << endl;
+			temp = temp->_next;
+		}
+		cout << endl;
 	}
+	cout << "Capacity: " << _capacity << endl;
 }
 
 template<typename K, typename T>
 void HashTable<K, T>::insert(K key, T value)
 {
+	size_t index = hash_function(key);
+	HashPair<K, T>* temp = &_data[index];
+	if (_data[index]._filled) {
+		while (temp->_next) {
+			temp = temp->_next;
+		}
+		temp->_next = new HashPair<K, T>(key, value);
+	}
+	else {
+		_data[index] = HashPair<K, T>(key, value);
+		_data[index]._filled = true;
+	}
+	_capacity++;
+	cout << hash_function(key) << endl;
+
 }
 
 template<typename K, typename T>
@@ -73,7 +106,20 @@ bool HashTable<K, T>::contains(T value)
 template<typename K, typename T>
 T* HashTable<K, T>::search(K key)
 {
-	return nullptr;
+	size_t index = hash_function(key);
+	if (!_data[index]._filled) {
+		return nullptr;
+	}
+	else {
+		HashPair<K, T>* temp = &_data[index];
+		while (temp) {
+			if (_data[index]._key == key) {
+				return &_data[index]._value;
+			}
+			temp = temp->_next;
+		}
+		return nullptr;
+	}
 }
 
 template<typename K, typename T>
@@ -88,4 +134,18 @@ int HashTable<K, T>::count(K key)
 	return 0;
 }
 
-int main() {}
+int main() {
+	HashTable<int, string> table(5);
+
+	table.insert(1, "one");
+	table.insert(228, "two");
+	table.insert(1000000, "three");
+	table.insert(153216, "four");
+	table.insert(550000, "five");
+	table.insert(153215, "dont know");
+
+	cout << "HashTable after insertions:" << endl;
+	table.print();
+
+	return 0;
+}
